34a2d81d1fc2662e3c7afeeda821123879c60d30
mahmutsbl1 <77998975+mahmutsbl1@users.noreply.github.com>
2021-10-11 00:15:35 +0300

    Add files via upload

---
+++ ilkdeneme.py
@@ -0,0 +1,8 @@
+matematik = input('matematik : ')
+turkce = input('turkce : ')
+kimya = input('kimya : ')
+fizik = input('fizik : ')
+biyoloji = input('biyoloji : ')
+tarih = input('tarih : ')
+toplam=float(matematik)+float(turkce)+float(kimya)+float(fizik)+float(biyoloji)+float(tarih)/25
+print("Toplam :{0} ".format(toplam))
